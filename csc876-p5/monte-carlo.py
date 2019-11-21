89d60a6896fbb2c8492af31cfe4e7c0fbcacb37c
AllenSun7 <sunlun7@yahoo.com>
2019-11-21 10:34:03 -0800

    csc876

---
+++ csc876-p5/monte-carlo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import math
+
+def andness_geo (n):
+    #calculator geometric mean andness for various value of n
+    #ag = (n/(n-1))-(n+1)/(n-1) * (n/(n+1))^n
+    total = 10000000000
+    ag = (n/(n-1)) - (n+1)/(n-1) * (n/(n+1))**n
+    ag_total = (total/(total-1)) - (total+1)/(total-1) * (total/(total+1))**total
+    print("ag[G;%.2f] = %.8f" %(n, ag))
+    print("ag[G;%.2f]/ag[G;%d] = %.8f" %(n, total, ag/ag_total))
+    return 
+
+def andness_indicator(n, total):
+    pass
+
+def weight_geo(n):
+    #calculator geometric mean weights for various value of n
+    #wg = (n+1)/(n-1) * (n/(n+1))^n - (1/(n-1))
+    wg = (n+1)/(n-1) * (n/(n+1))**n - (1/(n-1))
+    print("ag[G;%.2f] = %.8f" %(n, wg))
+
+def main():
+    andness_geo(2.0)
+
+if __name__ == "__main__":
+    main()
